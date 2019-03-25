15868023683f3ea2ecd09d1677824fc0e8324131
Hannah Kopplin <hannahkopplin@98-01-a7-ce-cb-b5.wlan.lsu.edu>
2019-03-25 13:55:13 -0500

    coords2

--- leaflet1/leaflet1.js
+++ leaflet1/leaflet1.js
@@ -2,10 +2,10 @@ let myLeaf = L.map('leaflet1').setView([32.18, -99.14], 4)
 L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png').addTo(myLeaf)
 let parisPoint = L.marker([48.85, 2.35]).addTo(myLeaf)
 let myTriangle = L.polygon([
-  [51.509, -0.08],
-  [51.503, -0.06],
-  [51.51, -0.047]
+  [49.4432, 1.1000],
+  [48.1173, 1.6778],
+  [47.3941, .06848]
 ]).addTo(myLeaf);
 
 parisPoint.bindPopup('Paris, France')
-myTriangle.bindPopup('A polygon')
+myTriangle.bindPopup('Rouen, Rennes, and Tours')
