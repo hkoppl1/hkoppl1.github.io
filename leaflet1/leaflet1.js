8de8f7041096a0a16d6edb3b97460a9714fbceec
Hannah Kopplin <hannahkopplin@Hannahs-MacBook-Air-3.local>
2019-03-25 13:38:07 -0500

    ,

--- leaflet1/leaflet1.js
+++ leaflet1/leaflet1.js
@@ -1,11 +1,11 @@
-let leaflet1 = L.map('leaflet1').setView([32.18, -99.14], 4)
-L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png').addTo(leaflet1)
-let nolaPoint = L.marker([30, -90]).addTo(leaflet1)
+let myLeaf = L.map('leaflet1').setView([32.18, -99.14], 4)
+L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png').addTo(myLeaf)
+let nolaPoint = L.marker([30, -90]).addTo(myLeaf)
 let myTriangle = L.polygon([
   [51.509, -0.08],
   [51.503, -0.06],
   [51.51, -0.047]
-]).addTo(leaflet1);
+]).addTo(myLeaf);
 
 nolaPoint.bindPopup('A marker')
 myTriangle.bindPopup('A polygon')
