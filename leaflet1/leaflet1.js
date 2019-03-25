04555e11a5924505fc9894c46d57cc4e54c31dc0
Hannah Kopplin <hannahkopplin@Hannahs-MacBook-Air-3.local>
2019-03-25 13:31:14 -0500

    map

--- leaflet1/leaflet1.js
+++ leaflet1/leaflet1.js
@@ -1,5 +1,5 @@
 let leaflet1 = L.map('leaflet1').setView([32.18, -99.14], 4)
-L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(leaflet1)
+L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png').addTo(leaflet1)
 let nolaPoint = L.marker([30, -90]).addTo(leaflet1)
 let myTriangle = L.polygon([
   [51.509, -0.08],
