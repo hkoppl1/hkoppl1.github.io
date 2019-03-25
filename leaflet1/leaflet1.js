2d8e78366153dec7b14c08379126c0f21a68a18a
Hannah Kopplin <hannahkopplin@Hannahs-MacBook-Air-3.local>
2019-03-25 13:27:14 -0500

    update6

--- leaflet1/leaflet1.js
+++ leaflet1/leaflet1.js
@@ -1,11 +1,11 @@
-let myMap = L.map('map1').setView([32.18, -99.14], 4)
-L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(myMap)
-let nolaPoint = L.marker([30, -90]).addTo(myMap)
+let leaflet1 = L.map('leaflet1').setView([32.18, -99.14], 4)
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(leaflet1)
+let nolaPoint = L.marker([30, -90]).addTo(leaflet1)
 let myTriangle = L.polygon([
   [51.509, -0.08],
   [51.503, -0.06],
   [51.51, -0.047]
-]).addTo(myMap);
+]).addTo(leaflet1);
 
 nolaPoint.bindPopup('A marker')
 myTriangle.bindPopup('A polygon')
