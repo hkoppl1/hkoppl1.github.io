791a68b9e3d1a57ddae9b84f91f31138ce451333
Hannah Kopplin <hannahkopplin@Hannahs-MacBook-Air-3.local>
2019-03-25 13:50:55 -0500

    coord

--- leaflet1/leaflet1.js
+++ leaflet1/leaflet1.js
@@ -1,11 +1,11 @@
 let myLeaf = L.map('leaflet1').setView([32.18, -99.14], 4)
 L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png').addTo(myLeaf)
-let nolaPoint = L.marker([30, -90]).addTo(myLeaf)
+let parisPoint = L.marker([48.85, 2.35]).addTo(myLeaf)
 let myTriangle = L.polygon([
   [51.509, -0.08],
   [51.503, -0.06],
   [51.51, -0.047]
 ]).addTo(myLeaf);
 
-nolaPoint.bindPopup('A marker')
+parisPoint.bindPopup('Paris, France')
 myTriangle.bindPopup('A polygon')
