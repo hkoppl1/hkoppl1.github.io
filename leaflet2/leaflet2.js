c629167fa72e0865ecf752fdb223c1b2d38fa759
Hannah N Kopplin <hkoppl1@lsu.edu>
2019-03-26 11:12:57 -0500

    coords2

--- leaflet2/leaflet2.js
+++ leaflet2/leaflet2.js
@@ -1,4 +1,4 @@
-let myLeaf = L.map('leaflet2').setView([39.0119, 98.4842], 4)
+let myLeaf = L.map('leaflet2').setView([39.0119, -98.4842], 4)
 L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png').addTo(myLeaf)
 
 L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
