0d2fa00147c3cefcefe98230044d2e5be83a2bc6
Hannah N Kopplin <hkoppl1@lsu.edu>
2019-03-26 11:53:01 -0500

    update1

--- leaflet2/leaflet2.js
+++ leaflet2/leaflet2.js
@@ -1,9 +1,14 @@
 let myLeaf = L.map('leaflet2').setView([39.0119, -98.4842], 4)
 L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png').addTo(myLeaf)
-
 L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
   layers: 'nexrad-n0r-900913',
   format: 'image/png',
   transparent: true,
   attribution: 'NOAA, Iowa State University'
+}).addTo(myLeaf)
+L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer/WMSServer?', {
+  layers: '1',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA'
 }).addTo(myLeaf)
