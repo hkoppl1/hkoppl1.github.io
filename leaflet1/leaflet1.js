746a1f6f0b84da6aafa9d1f035a52a39829d5777
hkoppl1 <48488052+hkoppl1@users.noreply.github.com>
2019-03-25 20:46:03 -0500

    Update leaflet1.js

--- leaflet1/leaflet1.js
+++ leaflet1/leaflet1.js
@@ -1,11 +1,16 @@
 let myLeaf = L.map('leaflet1').setView([32.18, -99.14], 4)
 L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png').addTo(myLeaf)
+let franceLine = L.line ([
+  [45.745805, 4.848651],
+  [44.816069, -0.560720]
+ ]).addTo(myLeaf)
 let parisPoint = L.marker([48.85, 2.35]).addTo(myLeaf)
 let myTriangle = L.polygon([
   [49.4432, 1.1000],
-  [48.1173, 1.6778],
-  [47.3941, .06848]
+  [48.099895, -1.670517],
+  [47.408520, .0691696]
 ]).addTo(myLeaf);
 
+franceLine.bindPopup('Lyon to Bordeaux')
 parisPoint.bindPopup('Paris, France')
 myTriangle.bindPopup('Rouen, Rennes, and Tours')
