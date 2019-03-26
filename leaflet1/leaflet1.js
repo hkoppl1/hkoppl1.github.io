cde78d705fc0db40167ec6d0c944b45d06df87d2
Hannah Kopplin <hannahkopplin@98-01-a7-ce-cb-b5.wlan.lsu.edu>
2019-03-25 20:54:06 -0500

    coord3

--- leaflet1/leaflet1.js
+++ leaflet1/leaflet1.js
@@ -3,7 +3,7 @@ L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png').addTo(myLeaf)
 let parisPoint = L.marker([48.85, 2.35]).addTo(myLeaf)
 let myTriangle = L.polygon([
   [49.4432, 1.1000],
-  [48.1173, 1.6778],
+  [48.1173, -1.6778],
   [47.3941, .06848]
 ]).addTo(myLeaf);
 
