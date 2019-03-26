962514f4270a1a8f686f6b972a29313c30dbb123
hkoppl1 <48488052+hkoppl1@users.noreply.github.com>
2019-03-25 20:47:42 -0500

    Update leaflet1.js

--- leaflet1/leaflet1.js
+++ leaflet1/leaflet1.js
@@ -3,7 +3,7 @@ L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png').addTo(myLeaf)
 let parisPoint = L.marker([48.85, 2.35]).addTo(myLeaf)
 let myTriangle = L.polygon([
   [49.4432, 1.1000],
-  [48.099895, -1.670517],
+  [48.099895, 1.670517],
   [47.408520, .0691696]
 ]).addTo(myLeaf);
 
