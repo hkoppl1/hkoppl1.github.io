ea6f5f7741c8b356a09b4031261c53b61a77be9a
Hannah Kopplin <hannahkopplin@98-01-a7-ce-cb-b5.wlan.lsu.edu>
2019-03-25 21:20:42 -0500

    tours

--- leaflet1/leaflet1.js
+++ leaflet1/leaflet1.js
@@ -4,7 +4,7 @@ let parisPoint = L.marker([48.85, 2.35]).addTo(myLeaf)
 let myTriangle = L.polygon([
   [49.4432, 1.1000],
   [48.1173, -1.6778],
-  [47.3941, .06848]
+  [47.399706, .0772170]
 ]).addTo(myLeaf);
 
 parisPoint.bindPopup('Paris, France')
