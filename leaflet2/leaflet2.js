94a1025d01450457e458080ef57b394eed9c1ec4
Hannah N Kopplin <hkoppl1@lsu.edu>
2019-03-26 11:01:24 -0500

    delete basemap

--- leaflet2/leaflet2.js
+++ leaflet2/leaflet2.js
@@ -1,6 +1,4 @@
 let myLeaf = L.map('leaflet2').setView([47.252931, 2.461270], 5)
-L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png').addTo(myLeaf)
-
 L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
   layers: 'nexrad-n0r-900913',
   format: 'image/png',
