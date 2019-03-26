6679fc0898a558e70c543903375450ae7dfac155
Hannah Kopplin <hannahkopplin@98-01-a7-ce-cb-b5.wlan.lsu.edu>
2019-03-25 22:01:42 -0500

    fix

--- leaflet1/leaflet1.js
+++ leaflet1/leaflet1.js
@@ -1,10 +1,6 @@
 let myLeaf = L.map('leaflet1').setView([47.252931, 2.461270], 5)
 L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png').addTo(myLeaf)
 let parisPoint = L.marker([48.85, 2.35]).addTo(myLeaf)
-let franceLine = polylinePoints([
-  [45.745805, 4.848651]
-  [44.816069, -0.560720]
-])
 let myTriangle = L.polygon([
   [49.4432, 1.1000],
   [48.1173, -1.6778],
