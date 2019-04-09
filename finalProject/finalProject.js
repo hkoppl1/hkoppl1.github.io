e66a9c232e90cccab1c7ad4ec0bad9bba2fa816a
Hannah N Kopplin <hkoppl1@lsu.edu>
2019-04-09 12:07:59 -0500

    final1rename

--- finalProject/finalProject.js
+++ finalProject/finalProject.js
@@ -1,4 +1,4 @@
-let restaurants = L.map('restaurants').setView([30.036033, -89.876431], 12)
+let restaurants = L.map('restaurants').setView([29.983565, -90.047463], 12)
 L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png').addTo(restaurants)
 let restaurantsUrl = 'https://opendata.arcgis.com/datasets/62501c695c614d1c99aaf2b113fca257_0.geojson'
 jQuery.getJSON(restaurantsUrl, function (data) {
@@ -8,10 +8,7 @@ jQuery.getJSON(restaurantsUrl, function (data) {
     if (businessType == '1105 - FULL SVC RESTAURANTS (TABLE SERVICE)' ) {restaurantColor = 'gold' }
     return {
       color: restaurantColor,
-      weight: 1.5,
-      stroke: true,
-      lineJoin: 'round',
-      fillOpacity: 0.3
+      weight: 1.5
     }
   }
   let onEachFeature = function (feature, layer) {
